aafc3f934f8101a50e330139b037fd2fa0ce1c68
Lilibeth <lilibethocando@gmail.com>
2024-10-27 19:57:34 -0500

    Testing some queries

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, Customer, Collection, OrderItem
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    queryset = OrderItem.objects.values("product_id").distinct()
+    queryset = Order.objects.all().select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
 
-    return render(request, "hello.html", {"products": queryset})
+    return render(request, "hello.html", {"orders": queryset})
