076872144aa402776de99d132bc3c15b61ca0d76
Lilibeth <lilibethocando@gmail.com>
2024-10-29 15:49:16 -0500

    Working with Views

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import product_list
+from .views import product_list, product_detail
 
 urlpatterns = [
-    path('products/', product_list)
+    path('products/', product_list),
+    path('products/<int:id>', product_detail)
 ]
--- store/views.py
+++ store/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render, HttpResponse
-from .models import Product
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
+
 
 @api_view()
 def product_list(request):
     queryset = Product.objects.all()
-    return Response("products.html", {"products": list(queryset)})
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
